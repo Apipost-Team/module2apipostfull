7ec8d55f2b0974dde81331b48d801c03bc3c059e
宣博文 <1030698842@qq.com>
2023-08-03 23:38:32 +0800

    fix bug

--- src/Module2ApiPostFull.ts
+++ src/Module2ApiPostFull.ts
@@ -444,8 +444,8 @@ const fullDataModel = (newJson: any, dataModel: any) => {
         let apisStr = JSON.stringify(newJson.apis);
         for (const model of newJson.dataModel) {
           if (model?.old_model_id) {
-            let reg = new RegExp(model.old_model_id, 'g')
-            dataModelStr = dataModelStr.replace(reg, model.model_id);
+            let reg = new RegExp(`\"${model.old_model_id}\"`, 'g')
+            dataModelStr = dataModelStr.replace(reg, `"${model.model_id}"`);
             apisStr= apisStr.replace(reg, model.model_id);
           }
         }
